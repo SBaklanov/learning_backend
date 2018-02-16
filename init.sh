28dc57581a1f8c235cbdf4cb720460924abc9813
SBaklanov <36495521+SBaklanov@users.noreply.github.com>
2018-02-16 19:36:12 +0300

    testing

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+cp /etc/nginx/nginx.conf /home/box/web/etc
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
